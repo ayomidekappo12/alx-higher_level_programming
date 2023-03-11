70f8c9c0764a259847cd8b7c510720ef40562a37
ayomidekappo12 <ayomidekappo12@gmail.com>
2023-03-11 01:20:36 -0800

    tasks 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,14 +3,14 @@
 if __name__ == "__main__":
     """
     calculate the sum,product,quotient and
-    difference
+    difference of 10 and 5.
     """
-    from calculator_1 import add, sub, div, mul
+    from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
 
-    print("{} + {} = {}".format(a,b, add(a,b)))
-    print("{} - {} = {}".format(a,b, sub(a,b)))
-    print("{} / {} = {}".format(a,b, div(a,b)))
-    print("{} * {} = {}".format(a,b, mul(a,b)))
+    print("{} + {} = {}".format(a, b, add(a,b)))
+    print("{} - {} = {}".format(a, b, sub(a,b)))
+    print("{} * {} = {}".format(a, b, mul(a,b)))
+    print("{} / {} = {}".format(a, b, div(a,b)))
