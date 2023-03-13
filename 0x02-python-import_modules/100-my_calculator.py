db4e1997a827cbdab2ffb43b783022a762da99e5
ayomidekappo12 <ayomidekappo12@gmail.com>
2023-03-13 21:10:16 +0000

    tasks 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,18 +1,34 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div 
-    import sys
+import sys
+from calculator_1 import add, sub, mul, div
 
-    if len(sys.argv) - 1 != 3:
+if __name__ == '__main__':
+    # Check if the number of arguments is correct
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
+    # Parse the arguments and convert them to integers
+    a, op, b = sys.argv[1:]
+    try:
+        a = int(a)
+        b = int(b)
+    except ValueError:
+        print("Error: arguments must be integers")
+        sys.exit(1)
+
+    # Perform the operation based on the operator
+    if op == "+":
+        result = add(a, b)
+    elif op == "-":
+        result = sub(a, b)
+    elif op == "*":
+        result = mul(a, b)
+    elif op == "/":
+        result = div(a, b)
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+    # Print the result
+    print("{} {} {} = {}".format(a, op, b, result))
