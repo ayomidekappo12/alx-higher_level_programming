471872752fef5327ef5a5a39c73179e3f14ef944
ayomidekappo12 <ayomidekappo12@gmail.com>
2023-03-13 21:15:59 +0000

    tasks 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,12 +3,10 @@ import sys
 from calculator_1 import add, sub, mul, div
 
 if __name__ == '__main__':
-    # Check if the number of arguments is correct
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    # Parse the arguments and convert them to integers
     a, op, b = sys.argv[1:]
     try:
         a = int(a)
@@ -17,7 +15,6 @@ if __name__ == '__main__':
         print("Error: arguments must be integers")
         sys.exit(1)
 
-    # Perform the operation based on the operator
     if op == "+":
         result = add(a, b)
     elif op == "-":
@@ -30,5 +27,4 @@ if __name__ == '__main__':
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    # Print the result
     print("{} {} {} = {}".format(a, op, b, result))
