ef0109ee0d52f1973febe23dcab2d36e90c49fb3
ayomidekappo12 <ayomidekappo12@gmail.com>
2023-04-01 18:49:17 +0000

    tasks 101 and 102 done

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
@@ -12,3 +13,4 @@ def magic_calculation(a, b):
             result = b + a
             break
     return (result)
+
