4b5d502a6985570d1a00a8b4115351e56ce2fb4d
ayomidekappo12 <ayomidekappo12@gmail.com>
2023-04-06 19:00:58 +0000

    tasks 29

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
-def magic_string(holbie=[]):
-    holbie += ["Holberton"]
-    return ", ".join(holbie)
+def magic_string(counter=[0]):
+    counter[0] += 1
+    return ', '.join(['BestSchool']*counter[0])
