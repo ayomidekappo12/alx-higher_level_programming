adc752ec2d36f6d1e4346c19a42b055e7ab368dd
ayomidekappo12 <ayomidekappo12@gmail.com>
2023-04-12 14:22:52 +0000

    tasks 100 to 101 done

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """Module base_geometry
-creates a class
+Creates a class
 """
 
 
 class BaseGeometry:
     """Class with public instance method"""
 
-
     def area(self):
-        """raise an exception with the message
-        'area() is not implemented'
+        """Raises an Exception with the message
+        'area() is  not implemented'
         """
 
         raise Exception('area() is not implemented')
