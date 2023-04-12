008e089c6683acde60c34d4a07b68cbfb1bfc4e0
ayomidekappo12 <ayomidekappo12@gmail.com>
2023-04-12 14:16:14 +0000

    tasks 6 t0 11 done

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module base_geometry
+creates a class
+"""
+
+
+class BaseGeometry:
+    """Class with public instance method"""
+
+
+    def area(self):
+        """raise an exception with the message
+        'area() is not implemented'
+        """
+
+        raise Exception('area() is not implemented')
---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""module base_Geometry
+creates a baseGeometry class
+"""
+
+
+class BaseGeometry:
+    """class with public instance method"""
+
+    def area(self):
+        """raises an Exception with the message
+        'area() is not implemented'
+        """
+
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """validates value"""
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
