373dc7d66295084140990b9eb7767ff677da42c2
ayomidekappo12 <ayomidekappo12@gmail.com>
2023-06-14 22:43:40 +0000

    added 0x12-javascript-warm_up

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,4 +3,5 @@
  Write a script that prints My number: <first argument converted in integer>
  if the first argument can be converted to an integer:
 */
-console.log(isNaN(Number(process.argv[2])) ? 'Not a number' : process.argv[2]);
+const num = Math.floor(Number(process.argv[2]));
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*
+ Write a script that prints 3 lines: (like 1-multi_languages.js)
+ but by using an array of string and a loop
+*/
+const loopArrays = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < loopArrays.length; i++) {
+    console.log(loopArrays[i]);
+}
