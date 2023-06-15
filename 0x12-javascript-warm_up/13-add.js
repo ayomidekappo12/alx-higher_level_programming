6b9638c4aab78854b850c0aad70c056ec9169d20
ayomidekappo12 <ayomidekappo12@gmail.com>
2023-06-15 19:10:45 +0000

    added 0x12-javascript-warm_up

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-//Write a function that returns the addition of 2 integers.
+// Write a function that returns the addition of 2 integers.
+// global add
 const add = (num1, num2) => num1 + num2;
 
 module.exports = {
-  add: add
+  add
 };
